ec58c6cbfa16aa27db6aa1dd1c5042bf75ae6190
Alessandro Caetano <alessandro.caetanob@gmail.com>
2017-09-25 07:31:39 -0300

    second version of lda

--- docs_classifier.py
+++ docs_classifier.py
@@ -5,14 +5,20 @@
 ########################################
 from __future__ import print_function
 
-from time import time
 from tqdm import tqdm
+from time import time
+
+import os
 
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
-from sklearn.decomposition import NMF, LatentDirichletAllocation
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
 
+########################################
+## print top 10 topics
+########################################
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
         message = "Topic #%d: " % topic_idx
@@ -25,39 +31,64 @@ STOP_WORDS = set(stopwords.words('english'))
 
 DOCS_DIR = './docs/'
 RAILS_DOCS_DIR = DOCS_DIR + 'rails/'
+PYTHON_DOCS_DIR = DOCS_DIR + 'python/'
 
-n_samples = 2000
-n_features = 1000
+def vectorize_docs(lang):
+    docs_vector = []
+    if lang == 'ruby':
+        with open(RAILS_DOCS_DIR+'gettingstarted.txt') as f:
+            raw = tqdm(f.readlines(), desc='Reading ruby document lines')
+            raw = [x.strip() for x in raw]
+            return raw
+    elif lang == 'python':
+        python_files = [os.path.join(root, name)
+             for root, dirs, files in os.walk(PYTHON_DOCS_DIR)
+             for name in files]
+        for file in python_files:
+            with open(file) as f:
+                raw = tqdm(f.readlines(), desc='Reading ' + file + ' document lines')
+                raw = [x.strip().lower() for x in raw]
+                docs_vector.append(raw)
+        return docs_vector
+    else:
+        return 0
+
+n_features = 10000
 n_components = 10
 n_top_words = 20
 
-with open(RAILS_DOCS_DIR+'gettingstarted.txt') as f:
-    raw = tqdm(f.readlines(), desc='Reading document lines')
-    raw = [x.strip() for x in raw]
+raw = vectorize_docs('python')
 
 tokenizer = RegexpTokenizer(r'\w+')
-tokens = tqdm(tokenizer.tokenize(str(raw)), desc='Tokenizing words')
-filtered_words = [word for word in tokens if word not in STOP_WORDS]
+stemmer = PorterStemmer()
 
-tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='english')
+t0 = time()
+tokens = tokenizer.tokenize(str(raw))
+print("Tokenizing done in %0.3fs." % (time() - t0))
 
-tfidf = tfidf_vectorizer.fit_transform(filtered_words)
+print('Number of words: ' + str(len(tokens)))
+
+t0 = time()
+stopped_tokens = [word for word in tokens if not word in STOP_WORDS]
+print("Removing stop words done in %0.3fs." % (time() - t0))
+
+t0 = time()
+stemmed_tokens = [stemmer.stem(word) for word in stopped_tokens]
+print("Stemming words done in %0.3fs." % (time() - t0))
+
 
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='english')
 
-tf = tf_vectorizer.fit_transform(filtered_words)
+t0 = time()
+tf = tf_vectorizer.fit_transform(tokens)
+print("Vectorizing done in %0.3fs." % (time() - t0))
 
-nmf = NMF(n_components=n_components, random_state=1, alpha=.1, l1_ratio=.5).fit(tfidf)
+lda = LatentDirichletAllocation(n_topics=n_components, max_iter=15, learning_method='online', learning_offset=50., random_state=0)
 
-tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-
-nmf = NMF(n_components=n_components, random_state=1, solver='cd', max_iter=1000, alpha=.1, l1_ratio=.5).fit(tfidf)
-
-tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-
-lda = LatentDirichletAllocation(n_topics=n_components, max_iter=10, learning_method='online', learning_offset=50., random_state=0)
-
+t0 = time()
 lda.fit(tf)
+print("LDA done in %0.3fs." % (time() - t0))
+
 
 tf_feature_names = tf_vectorizer.get_feature_names()
 print_top_words(lda, tf_feature_names, n_top_words)
